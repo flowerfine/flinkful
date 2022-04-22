fda165a6c8d106fd49111f97e2d3fd78771b5379
wangqi <wangqi@xinc818.group>
2022-04-22 14:59:03 +0800

    standalone session example.

---
+++ flinkful-examples/flinkful-cli-examples/flinkful-cli-frontend-examples/src/main/java/cn/sliew/flinkful/cli/frontend/example/StandaloneSessionExample.java
@@ -0,0 +1,56 @@
+package cn.sliew.flinkful.cli.frontend.example;
+
+import cn.sliew.flinkful.cli.base.CliClient;
+import cn.sliew.flinkful.cli.base.PackageJarJob;
+import cn.sliew.flinkful.cli.frontend.FrontendCliClient;
+import cn.sliew.flinkful.common.enums.DeploymentTarget;
+import org.apache.flink.client.deployment.executors.RemoteExecutor;
+import org.apache.flink.configuration.*;
+import org.apache.flink.runtime.jobgraph.SavepointRestoreSettings;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+public class StandaloneSessionExample {
+
+    private static String mysqlPath = "mysql-connector-java-8.0.28.jar";
+
+    private static String seatunnelHome = "apache-seatunnel-incubating-2.0.5-SNAPSHOT";
+    private static String seatunnelPath = seatunnelHome + "/lib/seatunnel-core-flink.jar";
+
+    public static void main(String[] args) throws Exception {
+        CliClient client = new FrontendCliClient();
+        client.submit(DeploymentTarget.STANDALONE_SESSION, buildConfiguration(), buildJarJob());
+    }
+
+    /**
+     * 通过 {@link PipelineOptions#JARS} 将任务 jar 包和对应的依赖都可以一起传到 JobManager。
+     * 通过这种方式，可以避免在 JobManager 手动添加 seatunnel-core-flink.jar 或 mysql-connector-java.jar
+     */
+    private static Configuration buildConfiguration() throws MalformedURLException {
+        Configuration configuration = new Configuration();
+        configuration.setString(JobManagerOptions.ADDRESS, "localhost");
+        configuration.setInteger(JobManagerOptions.PORT, 6123);
+        configuration.setInteger(RestOptions.PORT, 8081);
+        URL seatunnelURL = new File(seatunnelPath).toURL();
+        URL mysqlURL = new File(mysqlPath).toURL();
+        List<URL> jars = Arrays.asList(seatunnelURL, mysqlURL);
+        ConfigUtils.encodeCollectionToConfig(configuration, PipelineOptions.JARS, jars, Object::toString);
+        configuration.setString(DeploymentOptions.TARGET, RemoteExecutor.NAME);
+        return configuration;
+    }
+
+    private static PackageJarJob buildJarJob() {
+        PackageJarJob job = new PackageJarJob();
+        job.setJarFilePath(seatunnelPath);
+        job.setEntryPointClass("org.apache.seatunnel.SeatunnelFlink");
+        URL resource = StandaloneSessionExample.class.getClassLoader().getResource("flink_jdbc_file.conf");
+        job.setProgramArgs(new String[]{"--config", resource.getPath()});
+        job.setClasspaths(Arrays.asList());
+        job.setSavepointSettings(SavepointRestoreSettings.none());
+        return job;
+    }
+}
--- flinkful-examples/src/main/java/cn/sliew/flinkful/examples/Main.java
+++
@@ -1,8 +0,0 @@
-package cn.sliew.flinkful.examples;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
