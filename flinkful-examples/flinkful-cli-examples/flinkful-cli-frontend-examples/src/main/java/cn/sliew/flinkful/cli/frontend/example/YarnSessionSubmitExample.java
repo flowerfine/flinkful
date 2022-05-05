5c8c1b50053a76d9d5f154e6ce3ecb2b28e1cad0
wangqi <1942460489@qq.com>
2022-05-05 21:02:54 +0800

    flinkful-cli-frontend examples.

---
+++ flinkful-examples/flinkful-cli-examples/flinkful-cli-frontend-examples/src/main/java/cn/sliew/flinkful/cli/frontend/example/YarnSessionSubmitExample.java
@@ -0,0 +1,33 @@
+package cn.sliew.flinkful.cli.frontend.example;
+
+import cn.sliew.flinkful.cli.base.CliClient;
+import cn.sliew.flinkful.common.enums.DeploymentTarget;
+import cn.sliew.flinkful.common.examples.FlinkExamples;
+import org.apache.flink.configuration.*;
+import org.apache.flink.yarn.configuration.YarnConfigOptions;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class YarnSessionSubmitExample {
+
+    public static void main(String[] args) throws Exception {
+        CliClient client = Util.buildCliClient();
+        client.submit(DeploymentTarget.YARN_SESSION, buildConfiguration(), Util.buildJarJob());
+    }
+
+    private static Configuration buildConfiguration() throws MalformedURLException {
+        Configuration configuration = FlinkExamples.loadConfiguration();
+        configuration.setString(YarnConfigOptions.APPLICATION_ID, "application_1646981816129_0003");
+
+        configuration.set(YarnConfigOptions.PROVIDED_LIB_DIRS, Arrays.asList(new String[]{"hdfs://hadoop:9000/flink/1.13.6"}));
+        configuration.set(YarnConfigOptions.FLINK_DIST_JAR, "hdfs://hadoop:9000/flink/1.13.6/flink-dist_2.11-1.13.6.jar");
+
+        URL exampleUrl = new File(FlinkExamples.EXAMPLE_JAR).toURL();
+        ConfigUtils.encodeCollectionToConfig(configuration, PipelineOptions.JARS, Collections.singletonList(exampleUrl), Object::toString);
+        return configuration;
+    }
+}
