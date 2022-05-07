57e0507e612688b15228e3a6de98cdad956425e5
wangqi <1942460489@qq.com>
2022-05-07 08:51:07 +0800

    flinkful-rest-client examples.

--- flinkful-examples/flinkful-rest-examples/flinkful-rest-client-examples/src/main/java/cn/sliew/flinkful/rest/client/controller/JobController.java
+++ flinkful-examples/flinkful-rest-examples/flinkful-rest-client-examples/src/main/java/cn/sliew/flinkful/rest/client/controller/JobController.java
@@ -1,30 +1,187 @@
 package cn.sliew.flinkful.rest.client.controller;
 
-import cn.sliew.flinkful.rest.base.JobClient;
 import cn.sliew.flinkful.rest.base.RestClient;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.flink.runtime.messages.webmonitor.JobIdsWithStatusOverview;
+import org.apache.flink.runtime.messages.webmonitor.MultipleJobsDetails;
+import org.apache.flink.runtime.rest.handler.async.AsynchronousOperationInfo;
+import org.apache.flink.runtime.rest.handler.async.AsynchronousOperationResult;
+import org.apache.flink.runtime.rest.handler.async.TriggerResponse;
+import org.apache.flink.runtime.rest.messages.*;
+import org.apache.flink.runtime.rest.messages.checkpoints.CheckpointConfigInfo;
+import org.apache.flink.runtime.rest.messages.checkpoints.CheckpointStatistics;
+import org.apache.flink.runtime.rest.messages.checkpoints.CheckpointingStatistics;
+import org.apache.flink.runtime.rest.messages.checkpoints.TaskCheckpointStatisticsWithSubtaskDetails;
 import org.apache.flink.runtime.rest.messages.job.JobDetailsInfo;
+import org.apache.flink.runtime.rest.messages.job.JobExecutionResultResponseBody;
+import org.apache.flink.runtime.rest.messages.job.JobSubmitRequestBody;
+import org.apache.flink.runtime.rest.messages.job.JobSubmitResponseBody;
+import org.apache.flink.runtime.rest.messages.job.metrics.AggregatedMetricsResponseBody;
+import org.apache.flink.runtime.rest.messages.job.metrics.MetricCollectionResponseBody;
+import org.apache.flink.runtime.rest.messages.job.savepoints.SavepointInfo;
+import org.apache.flink.runtime.rest.messages.job.savepoints.SavepointTriggerRequestBody;
+import org.apache.flink.runtime.rest.messages.job.savepoints.stop.StopWithSavepointRequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
-@RequestMapping("/job")
-@Api(value = "/job", tags = "任务接口")
+@RequestMapping("/flink/jobs")
+@Api(value = "/jobs", tags = "jobs接口")
 public class JobController {
 
     @Autowired
     private RestClient restClient;
 
-    @GetMapping("jobDetail")
-    @ApiOperation("任务详情")
-    public CompletableFuture<JobDetailsInfo> jobDetail() throws IOException {
-        JobClient jobClient = restClient.job();
-        return jobClient.jobDetail("c6b8f69ef2d6ac1f35b0c506e7a38f7a");
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#jobs-overview
+     */
+    @GetMapping("overview")
+    @ApiOperation("")
+    public CompletableFuture<MultipleJobsDetails> jobsOverview() throws IOException {
+        return restClient.job().jobsOverview();
     }
+
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#jobs-1
+     */
+    @GetMapping
+    @ApiOperation("")
+    public CompletableFuture<JobIdsWithStatusOverview> jobs() throws IOException {
+        return restClient.job().jobs();
+    }
+
+    @GetMapping("/metrics")
+    @ApiOperation("")
+    public CompletableFuture<AggregatedMetricsResponseBody> jobsMetrics(@RequestParam(value = "get", required = false) Optional<String> get,
+                                                                        @RequestParam(value = "agg", required = false) Optional<String> agg,
+                                                                        @RequestParam(value = "jobs", required = false) Optional<String> jobs) throws IOException {
+        return restClient.job().jobsMetric(get, agg, jobs);
+    }
+
+    @GetMapping("{jobId}")
+    @ApiOperation("")
+    public CompletableFuture<JobDetailsInfo> job(@PathVariable("jobId") String jobId) throws IOException {
+        return restClient.job().jobDetail(jobId);
+    }
+
+    @GetMapping("{jobId}/config")
+    @ApiOperation("")
+    public CompletableFuture<JobConfigInfo> jobConfig(@PathVariable("jobId") String jobId) throws IOException {
+        return restClient.job().jobConfig(jobId);
+    }
+
+    @GetMapping("{jobId}/metrics")
+    @ApiOperation("")
+    public CompletableFuture<MetricCollectionResponseBody> jobMetrics(@PathVariable("jobId") String jobId,
+                                                                      @RequestParam(value = "get", required = false) Optional<String> get) throws IOException {
+        return restClient.job().jobMetrics(jobId, get);
+    }
+
+    @GetMapping("{jobId}/exceptions")
+    @ApiOperation("")
+    public CompletableFuture<JobExceptionsInfoWithHistory> jobExceptions(@PathVariable("jobId") String jobId,
+                                                                         @RequestParam(value = "maxExceptions", required = false) Optional<String> maxExceptions) throws IOException {
+        return restClient.job().jobException(jobId, maxExceptions);
+    }
+
+    @GetMapping("{jobId}/execution-result")
+    @ApiOperation("")
+    public CompletableFuture<JobExecutionResultResponseBody> jobExecutionResult(@PathVariable("jobId") String jobId) throws IOException {
+        return restClient.job().jobExecutionResult(jobId);
+    }
+
+    @GetMapping("{jobId}/accumulators")
+    @ApiOperation("")
+    public CompletableFuture<JobAccumulatorsInfo> jobAccumulators(@PathVariable("jobId") String jobId,
+                                                                  @RequestParam(value = "includeSerializedValue", required = false) Optional<Boolean> includeSerializedValue) throws IOException {
+        return restClient.job().jobAccumulators(jobId, includeSerializedValue);
+    }
+
+    @GetMapping("{jobId}/plan")
+    @ApiOperation("")
+    public CompletableFuture<JobPlanInfo> jobPlan(@PathVariable("jobId") String jobId) throws IOException {
+        return restClient.job().jobPlan(jobId);
+    }
+
+    @GetMapping("{jobId}/checkpoints")
+    @ApiOperation("")
+    public CompletableFuture<CheckpointingStatistics> jobCheckpoints(@PathVariable("jobId") String jobId) throws IOException {
+        return restClient.job().jobCheckpoints(jobId);
+    }
+
+    @GetMapping("{jobId}/checkpoints/config")
+    @ApiOperation("")
+    public CompletableFuture<CheckpointConfigInfo> jobCheckpointsConfig(@PathVariable("jobId") String jobId) throws IOException {
+        return restClient.job().jobCheckpointConfig(jobId);
+    }
+
+    @GetMapping("{jobId}/checkpoints/details/{checkpointId}")
+    @ApiOperation("")
+    public CompletableFuture<CheckpointStatistics> jobCheckpointDetail(@PathVariable("jobId") String jobId, @PathVariable("checkpointId") Long checkpointId) throws IOException {
+        return restClient.job().jobCheckpointDetail(jobId, checkpointId);
+    }
+
+    @GetMapping("{jobId}/checkpoints/details/{checkpointId}/subtasks/{vertexId}")
+    @ApiOperation("")
+    public CompletableFuture<TaskCheckpointStatisticsWithSubtaskDetails> jobCheckpointSubtaskDetail(@PathVariable("jobId") String jobId,
+                                                                                                    @PathVariable("checkpointId") Long checkpointId,
+                                                                                                    @PathVariable("vertexId") String vertexId) throws IOException {
+        return restClient.job().jobCheckpointSubtaskDetail(jobId, checkpointId, vertexId);
+    }
+
+    @PostMapping("/")
+    @ApiOperation("")
+    public CompletableFuture<JobSubmitResponseBody> submit(@RequestBody JobSubmitRequestBody requestBody) throws IOException {
+        return restClient.job().jobSubmit(requestBody);
+    }
+
+    @PatchMapping("{jobId}")
+    @ApiOperation("")
+    public CompletableFuture<EmptyResponseBody> terminate(@PathVariable("jobId") String jobId) throws IOException {
+        return restClient.job().jobTerminate(jobId, null);
+    }
+
+    @PostMapping("{jobId}/stop")
+    @ApiOperation("")
+    public CompletableFuture<TriggerResponse> stop(@PathVariable("jobId") String jobId, @RequestBody StopWithSavepointRequestBody requestBody) throws IOException {
+        return restClient.job().jobStop(jobId, requestBody);
+    }
+
+    /**
+     * Rescaling is temporarily disabled. See FLINK-12312
+     */
+    @PostMapping("{jobId}/rescaling")
+    @ApiOperation("")
+    public CompletableFuture<TriggerResponse> rescale(@PathVariable("jobId") String jobId,
+                                                      @RequestParam(value = "parallelism", defaultValue = "2") Integer parallelism) throws IOException {
+        return restClient.job().jobRescale(jobId, parallelism);
+    }
+
+    @GetMapping("{jobId}/rescaling/{triggerId}")
+    @ApiOperation("")
+    public CompletableFuture<AsynchronousOperationResult<AsynchronousOperationInfo>> rescaleResult(@PathVariable("jobId") String jobId,
+                                                                                                   @PathVariable("triggerId") String triggerId) throws IOException {
+        return restClient.job().jobRescaleResult(jobId, triggerId);
+    }
+
+    @PostMapping("{jobId}/savepoints")
+    @ApiOperation("")
+    public CompletableFuture<TriggerResponse> savepoint(@PathVariable("jobId") String jobId,
+                                                        @RequestBody SavepointTriggerRequestBody requestBody) throws IOException {
+        return restClient.job().jobSavepoint(jobId, requestBody);
+    }
+
+    @GetMapping("{jobId}/savepoints/{triggerId}")
+    @ApiOperation("")
+    public CompletableFuture<AsynchronousOperationResult<SavepointInfo>> savepointResult(@PathVariable("jobId") String jobId,
+                                                                                         @PathVariable("triggerId") String triggerId) throws IOException {
+        return restClient.job().jobSavepointResult(jobId, triggerId);
+    }
+
 }
--- flinkful-examples/flinkful-rest-examples/flinkful-rest-client-examples/src/main/java/cn/sliew/flinkful/rest/client/controller/TaskManagerController.java
+++ flinkful-examples/flinkful-rest-examples/flinkful-rest-client-examples/src/main/java/cn/sliew/flinkful/rest/client/controller/TaskManagerController.java
@@ -2,6 +2,7 @@ package cn.sliew.flinkful.rest.client.controller;
 
 import cn.sliew.flinkful.rest.base.RestClient;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.flink.runtime.rest.messages.LogListInfo;
 import org.apache.flink.runtime.rest.messages.job.metrics.AggregatedMetricsResponseBody;
 import org.apache.flink.runtime.rest.messages.job.metrics.MetricCollectionResponseBody;
@@ -16,42 +17,66 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
-@RequestMapping("/flink/task-manager")
-@Api(value = "/task-manager", tags = "TaskManager接口")
+@RequestMapping("/flink/taskmanagers")
+@Api(value = "/taskmanagers", tags = "TaskManager接口")
 public class TaskManagerController {
 
     @Autowired
     private RestClient restClient;
 
-    @GetMapping("/")
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#taskmanagers
+     */
+    @GetMapping
+    @ApiOperation("taskmanagers 概览")
     public CompletableFuture<TaskManagersInfo> taskManagers() throws IOException {
         return restClient.taskManager().taskManagers();
     }
 
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#taskmanagers-metrics
+     */
     @GetMapping("metrics")
+    @ApiOperation("taskmanagers metrics")
     public CompletableFuture<AggregatedMetricsResponseBody> taskMangersMetrics(@RequestParam(value = "get", required = false) Optional<String> get,
                                                                                @RequestParam(value = "agg", required = false) Optional<String> agg,
                                                                                @RequestParam(value = "taskmanagers", required = false) Optional<String> taskmanagers) throws IOException {
         return restClient.taskManager().taskManagersMetrics(get, agg, taskmanagers);
     }
 
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#taskmanagers-taskmanagerid
+     */
     @GetMapping("{taskManagerId}")
+    @ApiOperation("TaskManager 详情")
     public CompletableFuture<TaskManagerDetailsInfo> taskManagerDetail(@PathVariable("taskManagerId") String taskManagerId) throws IOException {
         return restClient.taskManager().taskManagerDetail(taskManagerId);
     }
 
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#taskmanagers-taskmanagerid-metrics
+     */
     @GetMapping("{taskManagerId}/metrics")
+    @ApiOperation("taskmanagers metrics")
     public CompletableFuture<MetricCollectionResponseBody> taskManagerMetrics(@PathVariable("taskManagerId") String taskManagerId,
                                                                               @RequestParam(value = "get", required = false) Optional<String> get) throws IOException {
         return restClient.taskManager().taskManagerMetrics(taskManagerId, get);
     }
 
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#taskmanagers-taskmanagerid-logs
+     */
     @GetMapping("{taskManagerId}/logs")
+    @ApiOperation("TaskManager 日志文件")
     public CompletableFuture<LogListInfo> taskManagerLogs(@PathVariable("taskManagerId") String taskManagerId) throws IOException {
         return restClient.taskManager().taskManagerLogs(taskManagerId);
     }
 
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#taskmanagers-taskmanagerid-thread-dump
+     */
     @GetMapping("{taskManagerId}/thread-dump")
+    @ApiOperation("dump TaskManager thread")
     public CompletableFuture<ThreadDumpInfo> taskManagerThreadDump(@PathVariable("taskManagerId") String taskManagerId) throws IOException {
         return restClient.taskManager().taskManagerThreadDump(taskManagerId);
     }
