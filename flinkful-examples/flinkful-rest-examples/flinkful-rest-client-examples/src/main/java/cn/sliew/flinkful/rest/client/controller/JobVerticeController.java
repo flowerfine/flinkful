c637e1babf6becc4b0d49f450ef917b5fec95170
wangqi <1942460489@qq.com>
2022-05-10 00:29:57 +0800

    flinkful-rest-client examples.

--- flinkful-examples/flinkful-rest-examples/flinkful-rest-client-examples/src/main/java/cn/sliew/flinkful/rest/client/controller/JobVerticeController.java
+++ flinkful-examples/flinkful-rest-examples/flinkful-rest-client-examples/src/main/java/cn/sliew/flinkful/rest/client/controller/JobVerticeController.java
@@ -3,9 +3,8 @@ package cn.sliew.flinkful.rest.client.controller;
 import cn.sliew.flinkful.rest.base.RestClient;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.flink.runtime.rest.messages.JobVertexAccumulatorsInfo;
-import org.apache.flink.runtime.rest.messages.JobVertexBackPressureInfo;
-import org.apache.flink.runtime.rest.messages.JobVertexDetailsInfo;
+import org.apache.flink.runtime.rest.messages.*;
+import org.apache.flink.runtime.rest.messages.job.SubtaskExecutionAttemptAccumulatorsInfo;
 import org.apache.flink.runtime.rest.messages.job.SubtaskExecutionAttemptDetailsInfo;
 import org.apache.flink.runtime.rest.messages.job.SubtasksAllAccumulatorsInfo;
 import org.apache.flink.runtime.rest.messages.job.metrics.MetricCollectionResponseBody;
@@ -121,16 +120,87 @@ public class JobVerticeController {
     public CompletableFuture<SubtaskExecutionAttemptDetailsInfo> jobVertexSubtaskDetail(
             @PathVariable("jobId") String jobId,
             @PathVariable("vertexId") String vertexId,
-            @RequestParam("subtaskIndex") Integer subtaskIndex) throws IOException {
+            @PathVariable("subtaskIndex") Integer subtaskIndex) throws IOException {
 
         return client.jobVertice().jobVertexSubtaskDetail(jobId, vertexId, subtaskIndex);
     }
 
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#jobs-jobid-vertices-vertexid-subtasks-subtaskindex-attempts-att
+     */
+    @GetMapping("{jobId}/vertices/{vertexId}/subtasks/{subtaskIndex}/attempts/{attempt}")
+    @ApiOperation("Returns details of an execution attempt of a subtask. Multiple execution attempts happen in case of failure/recovery.")
+    public CompletableFuture<SubtaskExecutionAttemptDetailsInfo> jobVertexSubtaskAttemptDetail(
+            @PathVariable("jobId") String jobId,
+            @PathVariable("vertexId") String vertexId,
+            @PathVariable("subtaskIndex") Integer subtaskIndex,
+            @PathVariable("attempt") Integer attempt) throws IOException {
 
+        return client.jobVertice().jobVertexSubtaskAttemptDetail(jobId, vertexId, subtaskIndex, attempt);
+    }
 
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#jobs-jobid-vertices-vertexid-subtasks-subtaskindex-attempts-att-1
+     */
+    @GetMapping("{jobId}/vertices/{vertexId}/subtasks/{subtaskIndex}/attempts/{attempt}/accumulators")
+    @ApiOperation("Returns the accumulators of an execution attempt of a subtask. Multiple execution attempts happen in case of failure/recovery.")
+    public CompletableFuture<SubtaskExecutionAttemptAccumulatorsInfo> jobVertexSubtaskAttemptAccumulators(
+            @PathVariable("jobId") String jobId,
+            @PathVariable("vertexId") String vertexId,
+            @PathVariable("subtaskIndex") Integer subtaskIndex,
+            @PathVariable("attempt") Integer attempt) throws IOException {
 
+        return client.jobVertice().jobVertexSubtaskAttemptAccumulators(jobId, vertexId, subtaskIndex, attempt);
+    }
 
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#jobs-jobid-vertices-vertexid-subtasks-subtaskindex-metrics
+     */
+    @GetMapping("{jobId}/vertices/{vertexId}/subtasks/{subtaskIndex}/metrics")
+    @ApiOperation("Provides access to subtask metrics.")
+    public CompletableFuture<MetricCollectionResponseBody> jobVertexSubtaskMetrics(
+            @PathVariable("jobId") String jobId,
+            @PathVariable("vertexId") String vertexId,
+            @PathVariable("subtaskIndex") Integer subtaskIndex,
+            @RequestParam("get") String get) throws IOException {
 
+        return client.jobVertice().jobVertexSubtaskMetrics(jobId, vertexId, subtaskIndex, get);
+    }
 
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#jobs-jobid-vertices-vertexid-subtasktimes
+     */
+    @GetMapping("{jobId}/vertices/{vertexId}/subtasktimes")
+    @ApiOperation("Returns time-related information for all subtasks of a task.")
+    public CompletableFuture<SubtasksTimesInfo> jobVertexSubtaskTimes(
+            @PathVariable("jobId") String jobId,
+            @PathVariable("vertexId") String vertexId) throws IOException {
+
+        return client.jobVertice().jobVertexSubtaskTimes(jobId, vertexId);
+    }
+
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#jobs-jobid-vertices-vertexid-subtasktimes
+     */
+    @GetMapping("{jobId}/vertices/{vertexId}/taskmanagers")
+    @ApiOperation("Returns task information aggregated by task manager.")
+    public CompletableFuture<JobVertexTaskManagersInfo> jobVertexTaskManagers(
+            @PathVariable("jobId") String jobId,
+            @PathVariable("vertexId") String vertexId) throws IOException {
+
+        return client.jobVertice().jobVertexTaskManagers(jobId, vertexId);
+    }
+
+    /**
+     * https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/ops/rest_api/#jobs-jobid-vertices-vertexid-watermarks
+     */
+    @GetMapping("{jobId}/vertices/{vertexId}/watermarks")
+    @ApiOperation("Returns the watermarks for all subtasks of a task.")
+    public CompletableFuture<MetricCollectionResponseBody> jobVertexWatermarks(
+            @PathVariable("jobId") String jobId,
+            @PathVariable("vertexId") String vertexId) throws IOException {
+
+        return client.jobVertice().jobVertexWatermarks(jobId, vertexId);
+    }
 
 }
