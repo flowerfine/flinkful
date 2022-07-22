9eb4279b362205a349097afd183bbb46fa0da638
wangqi <1942460489@qq.com>
2022-07-22 08:40:44 +0800

    feature: change flink on Native Kubernetes service to loadbalance

--- flinkful-examples/flinkful-cli-examples/flinkful-cli-descriptor-examples/src/main/java/cn/sliew/flinkful/cli/descriptor/example/session/NativeKubernetesSessionCreateExample.java
+++ flinkful-examples/flinkful-cli-examples/flinkful-cli-descriptor-examples/src/main/java/cn/sliew/flinkful/cli/descriptor/example/session/NativeKubernetesSessionCreateExample.java
@@ -26,8 +26,7 @@ public class NativeKubernetesSessionCreateExample {
 
     private static Configuration buildSessionConfiguration() {
         Configuration configuration = FlinkExamples.loadConfiguration();
-        configuration.set(KubernetesConfigOptions.REST_SERVICE_EXPOSED_TYPE, KubernetesConfigOptions.ServiceExposedType.NodePort);
-//        configuration.set(KubernetesConfigOptions.REST_SERVICE_EXPOSED_NODE_PORT_ADDRESS_TYPE, KubernetesConfigOptions.NodePortAddressType.ExternalIP);
+        configuration.set(KubernetesConfigOptions.REST_SERVICE_EXPOSED_TYPE, KubernetesConfigOptions.ServiceExposedType.LoadBalancer);
         configuration.setLong(JobManagerOptions.TOTAL_PROCESS_MEMORY.key(), MemorySize.ofMebiBytes(2048).getBytes());
         configuration.setLong(TaskManagerOptions.TOTAL_PROCESS_MEMORY.key(), MemorySize.ofMebiBytes(2048).getBytes());
         configuration.set(TaskManagerOptions.NUM_TASK_SLOTS, 2);
