4aab4e6d503e56c6e760b56d007792a7dc35ba6e
wangqi <1942460489@qq.com>
2022-10-28 21:10:40 +0800

    feature: add seatunnel v2 connectors examples

--- flinkful-examples/flinkful-cli-examples/flinkful-cli-descriptor-examples/src/main/java/cn/sliew/flinkful/cli/descriptor/example/StandaloneSession2Example.java
+++ flinkful-examples/flinkful-cli-examples/flinkful-cli-descriptor-examples/src/main/java/cn/sliew/flinkful/cli/descriptor/example/StandaloneSession2Example.java
@@ -12,17 +12,16 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class StandaloneSession2Example {
 
-    //    private static String mysqlPath = "/Users/wangqi/Documents/software/repository/mysql/mysql-connector-java/8.0.28/mysql-connector-java-8.0.28.jar";
-    private static String mysqlPath = "/Library/Maven/repository/mysql/mysql-connector-java/8.0.28/mysql-connector-java-8.0.28.jar";
-
-    private static String seatunnelHome = "/Users/wangqi/Downloads/apache-seatunnel-incubating-2.0.5-SNAPSHOT";
-    private static String seatunnelPath = seatunnelHome + "/lib/seatunnel-core-flink.jar";
-
+    private static String SEATUNNEL_HOME = "/Users/wangqi/Documents/software/seatunnel/apache-seatunnel-incubating-2.2.0-beta";
+    private static String CORE_JAR = SEATUNNEL_HOME + "/lib/seatunnel-flink-starter.jar";
+    private static String FAKE_CONNECTOR_JAR = SEATUNNEL_HOME + "/connectors/seatunnel/connector-fake-2.2.0-beta.jar";
+    private static String CONSOLE_CONNECTOR_JAR = SEATUNNEL_HOME + "/connectors/seatunnel/connector-console-2.2.0-beta.jar";
 
     public static void main(String[] args) throws Exception {
         CliClient client = new DescriptorCliClient();
@@ -39,17 +38,18 @@ public class StandaloneSession2Example {
         configuration.setString(JobManagerOptions.ADDRESS, "localhost");
         configuration.setInteger(JobManagerOptions.PORT, 6123);
         configuration.setInteger(RestOptions.PORT, 8081);
-        URL mysqlURL = new File(mysqlPath).toURL();
-        List<URL> jars = Collections.singletonList(mysqlURL);
+        URL fakeConnector = new File(FAKE_CONNECTOR_JAR).toURL();
+        URL consoleConnector = new File(CONSOLE_CONNECTOR_JAR).toURL();
+        List<URL> jars = Arrays.asList(fakeConnector, consoleConnector);
         ConfigUtils.encodeCollectionToConfig(configuration, PipelineOptions.JARS, jars, Object::toString);
         return configuration;
     }
 
     private static PackageJarJob buildJarJob() {
         PackageJarJob job = new PackageJarJob();
-        job.setJarFilePath("file://" + seatunnelPath);
-        job.setEntryPointClass("org.apache.seatunnel.SeatunnelFlink");
-        URL resource = StandaloneSession2Example.class.getClassLoader().getResource("flink_jdbc_file.conf");
+        job.setJarFilePath("file://" + CORE_JAR);
+        job.setEntryPointClass("org.apache.seatunnel.core.starter.flink.SeatunnelFlink");
+        URL resource = StandaloneSession2Example.class.getClassLoader().getResource("fake_console.json");
         job.setProgramArgs(new String[]{"--config", resource.getPath()});
         job.setClasspaths(Collections.emptyList());
         job.setSavepointSettings(SavepointRestoreSettings.none());
