de1d0e21410163f6e2dbca95f131c85d6842d8ae
wangqi <1942460489@qq.com>
2022-11-01 08:36:20 +0800

    feature: flink sql gateway

---
+++ flinkful-sql-gateway/src/main/java/cn/sliew/flinkful/sql/gateway/controller/SessionController.java
@@ -0,0 +1,13 @@
+package cn.sliew.flinkful.sql.gateway.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/session")
+@Api(value = "/session", tags = "Session接口")
+public class SessionController {
+
+
+}
