95c0ceb767368cab35360a2cc02393f93cad1b59
wangqi <wangqi@xinxuan.net>
2025-04-29 15:41:37 +0800

    feature: update flink operator crd

--- flinkful-kubernetes/flinkful-kubernetes-operator/src/main/java/cn/sliew/flinkful/kubernetes/operator/crd/spec/JobReference.java
+++ flinkful-kubernetes/flinkful-kubernetes-operator/src/main/java/cn/sliew/flinkful/kubernetes/operator/crd/spec/JobReference.java
@@ -1,8 +1,5 @@
 package cn.sliew.flinkful.kubernetes.operator.crd.spec;
 
-
-import cn.sliew.flinkful.kubernetes.operator.crd.AbstractFlinkResource;
-import cn.sliew.flinkful.kubernetes.operator.crd.CrdConstants;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,27 +25,4 @@ public class JobReference {
      * Name of the Flink resource.
      */
     private String name;
-
-    public static JobReference fromFlinkResource(AbstractFlinkResource<?, ?> flinkResource) {
-        var result = new JobReference();
-        result.setName(flinkResource.getMetadata().getName());
-
-        if (flinkResource instanceof FlinkDeployment) {
-            result.setKind(JobKind.FLINK_DEPLOYMENT);
-        } else if (flinkResource instanceof FlinkSessionJob) {
-            result.setKind(JobKind.FLINK_SESSION_JOB);
-        }
-
-        return result;
-    }
-
-    public String toString() {
-        String kindString = kind.name();
-        if (kind == JobKind.FLINK_DEPLOYMENT) {
-            kindString = CrdConstants.KIND_FLINK_DEPLOYMENT;
-        } else if (kind == JobKind.FLINK_SESSION_JOB) {
-            kindString = CrdConstants.KIND_SESSION_JOB;
-        }
-        return String.format("%s (%s)", name, kindString);
-    }
 }
